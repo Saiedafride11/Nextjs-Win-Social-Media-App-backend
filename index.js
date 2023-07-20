65b618e51b03bb960e268478de70736ca13babff
Coders Key <designer.afride@gmail.com>
2023-07-20 12:46:35 +0600

    file upload error check-1

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const cors = require('cors');
 // const bodyParser = require('body-parser');
 const ObjectId = require('mongodb').ObjectId;
-const fileUpload = require('express-fileupload');
+// const fileUpload = require('express-fileupload');
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -13,7 +13,7 @@ app.use(cors())
 // app.use(bodyParser.json())
 // or
 app.use(express.json())
-app.use(fileUpload());
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u9lnx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -126,3 +126,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log('Win-Social-Media Server Running', port)
 })
+
+// app.use(fileUpload());
